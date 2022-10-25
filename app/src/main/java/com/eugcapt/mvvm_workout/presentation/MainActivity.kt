8e434b8ba36dfb4cc45779ad79df3ff087d0dde3
eugcapt <zhk.sem@yandex.ru>
2022-10-25 22:13:06 +0300

    added view model for MainActivity and its factory

--- app/src/main/java/com/eugcapt/mvvm_workout/presentation/MainActivity.kt
+++ app/src/main/java/com/eugcapt/mvvm_workout/presentation/MainActivity.kt
@@ -8,29 +8,19 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.eugcapt.mvvm_workout.R
 import com.eugcapt.mvvm_workout.data.repository.UserRepositoryImpl
+import com.eugcapt.mvvm_workout.data.storage.models.User
 import com.eugcapt.mvvm_workout.data.storage.sharedprefs.SharedPrefsUserStorage
+import com.eugcapt.mvvm_workout.domain.models.UserName
 
 
 class MainActivity : AppCompatActivity() {
 
    // private val userStorage = UserStorage()
-   private val userRepository by lazy(LazyThreadSafetyMode.NONE) {
-       UserRepositoryImpl(userStorage = SharedPrefsUserStorage(context = applicationContext))
-   }
 
-    private val getUserNameUseCase by lazy(LazyThreadSafetyMode.NONE){
-        com.eugcapt.mvvm_workout.domain.usecase.GetUserNameUseCase(
-            userRepository = userRepository
-        )
-    }
-    private val saveUserNameUseCase by lazy(LazyThreadSafetyMode.NONE){
-        com.eugcapt.mvvm_workout.domain.usecase.SaveUserNameUseCase(
-            userRepository = userRepository
-        )
-    }
 
     private lateinit var vm: MainViewModel
 
@@ -42,25 +32,25 @@ class MainActivity : AppCompatActivity() {
 
         Log.e("AAA", "Activity created")
 
-        vm = ViewModelProvider(this).get(MainViewModel::class.java)
+        vm = ViewModelProvider(this, MainViewModelFactory(this)).get(MainViewModel::class.java)
 
         val dataTextView = findViewById<TextView>(R.id.dataTextView)
         val dataEditTextVIew = findViewById<EditText>(R.id.dataEditTextVIew)
         val sendButton = findViewById<Button>(R.id.sendButton)
         val receiveButton = findViewById<Button>(R.id.receiveButton)
 
-        //val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+       vm.resultLive.observe(this, Observer {
+           dataTextView.text = it
+       })
+
 
         receiveButton.setOnClickListener{
-            val userName: com.eugcapt.mvvm_workout.domain.models.UserName = getUserNameUseCase.execute()
-            "${userName.firstName} ${userName.lastName}".also { dataTextView.text = it }
+            val text = dataEditTextVIew.text.toString()
+            vm.save(text)
         }
 
         sendButton.setOnClickListener {
-            val text = dataEditTextVIew.text.toString()
-            val params = com.eugcapt.mvvm_workout.domain.models.SaveUserNameParam(name = text)
-            val result: Boolean = saveUserNameUseCase.execute(param = params)
-            dataTextView.text = "save result = $result"
+          vm.load()
         }
     }
 }
--- app/src/main/java/com/eugcapt/mvvm_workout/presentation/MainViewModel.kt
+++ app/src/main/java/com/eugcapt/mvvm_workout/presentation/MainViewModel.kt
@@ -1,9 +1,20 @@
 package com.eugcapt.mvvm_workout.presentation
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.eugcapt.mvvm_workout.domain.models.SaveUserNameParam
+import com.eugcapt.mvvm_workout.domain.models.UserName
+import com.eugcapt.mvvm_workout.domain.usecase.GetUserNameUseCase
+import com.eugcapt.mvvm_workout.domain.usecase.SaveUserNameUseCase
 
-class MainViewModel: ViewModel() {
+class MainViewModel(private val getUserNameUseCase: GetUserNameUseCase,
+                    private val saveUserNameUseCase: SaveUserNameUseCase) : ViewModel() {
+
+
+   private val resultLiveMutable = MutableLiveData<String>()
+    val resultLive: LiveData<String> = resultLiveMutable
 
     init {
         Log.e("AAA", "VM created")
@@ -12,6 +23,21 @@ class MainViewModel: ViewModel() {
     override fun onCleared() {
         Log.e("AAA", "VM cleared")
         super.onCleared()
+    }
+
+
+
+    fun save(text: String) {
+        val params = SaveUserNameParam(name = text)
+        val resultData: Boolean = saveUserNameUseCase.execute(param = params)
+        resultLiveMutable.value = "save result = $resultData"
+
+    }
+
+    fun load(){
+        val userName: UserName = getUserNameUseCase.execute()
+        resultLiveMutable.value  =  "${userName.firstName} ${userName.lastName}"
+
     }
 
 }
